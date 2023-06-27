87569dc27527d474c0a9ae89eec151841e423d7c
Nnamani John <nnamanijohn123@gmail.com>
2023-06-27 21:22:33 +0100

    task 7 -9

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,17 +2,17 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.all([
-    uploadPhoto(fileName),
+  const promises = [
     signUpUser(firstName, lastName),
-  ]).then((values) => [
-    {
-      status: values[0].status,
-      value: values[0].body,
-    },
-    {
-      status: values[1].status,
-      value: values[1].body,
-    },
-  ]);
+    uploadPhoto(fileName),
+  ];
+
+  return Promise.allSettled(promises)
+    .then((results) => {
+      const formattedResults = results.map((result) => ({
+        status: result.status,
+        value: result.status === 'fulfilled' ? result.value : result.reason,
+      }));
+      return formattedResults;
+    });
 }
