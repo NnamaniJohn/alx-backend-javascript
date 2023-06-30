6b7aee3ceef4560982ba930e4cce9e3ebba65880
Nnamani John <nnamanijohn123@gmail.com>
2023-06-30 14:07:11 +0100

    task 7 - 100

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,17 +1,19 @@
-import Car from './10-car.js';
+import Car from './10-car';
 
 export default class EVCar extends Car {
-    constructor(brand, motor, color, range) {
-        super(brand, motor, color);
-        this._range = range;
-    }
-    
-    get range() {
-        return this._range;
-    }
-    
-    cloneCar() {
-        const { brand, motor, color, range } = this;
-        return new Car(brand, motor, color, range);
-    }
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this._range = range;
+  }
+
+  get range() {
+    return this._range;
+  }
+
+  cloneCar() {
+    const {
+      brand, motor, color, range,
+    } = this;
+    return new Car(brand, motor, color, range);
+  }
 }
