8354d74c26ac663eff0efffd5d3625d1ee5e2f4b
Nnamani John <nnamanijohn123@gmail.com>
2024-03-05 10:35:58 +0100

    fix errors

--- 0x00-ES6_basic/main.js
+++ 0x00-ES6_basic/main.js
@@ -1,14 +1,3 @@
-import createEmployeesObject from './11-createEmployeesObject.js';
-import createReportObject from './12-createReportObject.js';
-import createIteratorObject from './100-createIteratorObject.js';
-import iterateThroughObject from './101-iterateThroughObject.js';
+import getSanFranciscoDescription from './6-string-interpolation';
 
-const employees = {
-  ...createEmployeesObject('engineering', ['Bob', 'Jane']),
-  ...createEmployeesObject('marketing', ['Sylvie']),
-};
-
-const report = createReportObject(employees);
-const reportWithIterator = createIteratorObject(report);
-
-console.log(iterateThroughObject(reportWithIterator));
+console.log(getSanFranciscoDescription());
