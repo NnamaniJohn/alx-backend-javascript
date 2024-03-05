5c3ca18f7832c6470fb18a6d7b0aa4aa7420580e
Nnamani John <nnamanijohn123@gmail.com>
2024-03-05 11:37:25 +0100

    es6 promise

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,9 @@
+import { uploadPhoto, createUser } from './utils.js';
+
+export default function handleProfileSignup() {
+  createUser().then((user) => {
+    uploadPhoto().then((res) => {
+      console.log(res.body, user.firstName, user.lastName);
+    }).catch(() => console.log('Signup system offline'));
+  }).catch(() => console.log('Signup system offline'));
+}
