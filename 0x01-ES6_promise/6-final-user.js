aa2d05f53a4886e4c85683b8e38ae972e1fbd593
Nnamani John <nnamanijohn123@gmail.com>
2024-03-06 11:44:57 +0100

    task 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,6 +1,14 @@
 import signUpUser from './4-user-promise.js';
 import uploadPhoto from './5-photo-reject.js';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  const user = signUpUser(firstName, lastName);
+  const file = uploadPhoto(fileName);
 
+  const [userRes, fileRes] = await Promise.allSettled([user, file]);
+
+  return [
+    { status: userRes.status, value: userRes.value },
+    { status: fileRes.status, value: fileRes.value },
+  ];
 }
