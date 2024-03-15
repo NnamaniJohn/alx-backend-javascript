9921a32d93492960548aec00c382764da01c6fbd
Nnamani John <nnamanijohn123@gmail.com>
2024-03-15 12:12:00 +0100

    data manipulation

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,7 +2,7 @@ export default function cleanSet(set, startString) {
   if (startString === '') return '';
   const arr = [];
   for (const item of set) {
-    if (item.startsWith(startString)) arr.push(item.replace(startString, ''));
+    if (item && item.startsWith(startString)) arr.push(item.replace(startString, ''));
   }
   return arr.join('-');
 }
