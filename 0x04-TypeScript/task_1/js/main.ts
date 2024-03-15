7ef06c82bd047a06626b27c49c2e52de3b661b49
Nnamani John <nnamanijohn123@gmail.com>
2024-03-15 11:07:44 +0100

    typescript

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -6,3 +6,45 @@ interface Teacher {
     readonly location: string;
     [key: string]: any;
 }
+
+interface Directors extends Teacher {
+    numberOfReports: number;
+}
+
+interface PrintTeacherFunction {
+    (firstName: string, lastName: string): string;
+}
+const printTeacher: PrintTeacherFunction = (firstName, lastName) => {
+    const firstInitial = firstName.charAt(0).toUpperCase();
+    return `${firstInitial}. ${lastName}`;
+};
+
+interface StudentConstructor {
+    firstName: string;
+    lastName: string;
+}
+
+// Define the interface for the StudentClass
+interface StudentClassInterface {
+    workOnHomework(): string;
+    displayName(): string;
+}
+
+// Implement the StudentClass
+class StudentClass implements StudentClassInterface {
+    private firstName: string;
+    private lastName: string;
+
+    constructor({ firstName, lastName }: StudentConstructor) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    workOnHomework(): string {
+        return "Currently working";
+    }
+
+    displayName(): string {
+        return this.firstName;
+    }
+}
