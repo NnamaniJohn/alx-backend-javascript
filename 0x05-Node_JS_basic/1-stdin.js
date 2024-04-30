f7000c466916581688b04b95e20ab4641e131222
Nnamani John <nnamanijohn123@gmail.com>
2024-04-30 11:42:15 +0100

    node basic

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,8 +1,7 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on('data', (input) => {
-  const name = input.toString().trim();
-  console.log(`Your name is: ${name}`);
+process.stdin.on('data', (data) => {
+  process.stdout.write(`Your name is: ${data}`);
 });
 
 process.stdin.on('end', () => {
